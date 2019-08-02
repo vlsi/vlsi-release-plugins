208734b6e9d67c11d0d697f883f3e23107284820
Vladimir Sitnikov <sitnikov.vladimir@gmail.com>
2019-08-02 10:40:19 +0300

    Add dependency checksum validation

--- buildSrc/settings.gradle.kts
+++ buildSrc/settings.gradle.kts
@@ -32,3 +32,38 @@ for (project in rootProject.children) {
     assert(project.projectDir.isDirectory)
     assert(project.buildFile.isFile)
 }
+
+buildscript {
+    dependencies {
+        classpath("com.github.vlsi.gradle:checksum-dependency-plugin:1.18.0")
+    }
+    repositories {
+        gradlePluginPortal()
+    }
+}
+
+// Note: we need to verify the checksum for checksum-dependency-plugin itself
+val expectedSha512 =
+    "14CF9F9CA05397DBB6B94AEC424C11916E4BC2CE477F439F50408459EADCAB14C6243365BA7499C395192BC14ED9164FB1862CE9E1A3B5DAAD040FA218201A39"
+
+fun File.sha512(): String {
+    val md = java.security.MessageDigest.getInstance("SHA-512")
+    forEachBlock { buffer, bytesRead ->
+        md.update(buffer, 0, bytesRead)
+    }
+    return BigInteger(1, md.digest()).toString(16).toUpperCase()
+}
+
+val checksumDependencyJar: File = buildscript.configurations["classpath"].resolve().first()
+val actualSha512 = checksumDependencyJar.sha512()
+if (actualSha512 != expectedSha512) {
+    throw GradleException(
+        """
+        Checksum mismatch for $checksumDependencyJar
+        Expected: $expectedSha512
+          Actual: $actualSha512
+        """.trimIndent()
+    )
+}
+
+apply(plugin = "com.github.vlsi.checksum-dependency")
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -25,3 +25,40 @@ include(
     "plugins:license-gather-plugin",
     "plugins:stage-vote-release-plugin"
 )
+
+buildscript {
+    dependencies {
+        classpath("com.github.vlsi.gradle:checksum-dependency-plugin:1.18.0")
+        // Note: replace with below to use locally-built jar file (extra logging is not released yet)
+        // classpath(files("plugins/checksum-dependency-plugin/build/libs/checksum-dependency-plugin-1.18.0.jar"))
+    }
+    repositories {
+        gradlePluginPortal()
+    }
+}
+
+// Note: we need to verify the checksum for checksum-dependency-plugin itself
+val expectedSha512 =
+    "14CF9F9CA05397DBB6B94AEC424C11916E4BC2CE477F439F50408459EADCAB14C6243365BA7499C395192BC14ED9164FB1862CE9E1A3B5DAAD040FA218201A39"
+
+fun File.sha512(): String {
+    val md = java.security.MessageDigest.getInstance("SHA-512")
+    forEachBlock { buffer, bytesRead ->
+        md.update(buffer, 0, bytesRead)
+    }
+    return BigInteger(1, md.digest()).toString(16).toUpperCase()
+}
+
+val checksumDependencyJar: File = buildscript.configurations["classpath"].resolve().first()
+val actualSha512 = checksumDependencyJar.sha512()
+if (actualSha512 != expectedSha512) {
+    throw GradleException(
+        """
+        Checksum mismatch for $checksumDependencyJar
+        Expected: $expectedSha512
+          Actual: $actualSha512
+        """.trimIndent()
+    )
+}
+
+apply(plugin = "com.github.vlsi.checksum-dependency")
