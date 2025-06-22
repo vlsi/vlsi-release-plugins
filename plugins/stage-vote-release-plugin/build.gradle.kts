ce368b2a2d6c7716896714fb1bc0a71545efa75b
Vladimir Sitnikov <sitnikov.vladimir@gmail.com>
2025-06-22 09:10:25 +0300

    chore: add constraints to bump transitive versions to resolve CVEs

--- plugins/stage-vote-release-plugin/build.gradle.kts
+++ plugins/stage-vote-release-plugin/build.gradle.kts
@@ -21,6 +21,14 @@ plugins {
 }
 
 dependencies {
+    constraints {
+        api("commons-beanutils:commons-beanutils:1.11.0")
+        api("commons-codec:commons-codec:1.18.0")
+        api("commons-collections:commons-collections:3.2.2")
+        api("net.sourceforge.nekohtml:nekohtml:1.9.22")
+        api("org.apache.httpcomponents:httpclient:4.5.13")
+        api("xerces:xercesImpl:2.12.2")
+    }
     implementation(project(":plugins:crlf-plugin"))
     implementation(project(":plugins:license-gather-plugin"))
     implementation(project(":plugins:gradle-extensions-plugin"))
