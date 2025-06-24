0e6c41373caca33acce910b1c766fa6a2d5bbed2
Vladimir Sitnikov <sitnikov.vladimir@gmail.com>
2025-06-24 17:55:01 +0300

    chore: add org.gradle.toolchains.foojay-resolver-convention for automatic Java provisioning

--- buildSrc/settings.gradle.kts
+++ buildSrc/settings.gradle.kts
@@ -21,6 +21,10 @@ pluginManagement {
     }
 }
 
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention")
+}
+
 dependencyResolutionManagement {
     repositories {
         mavenCentral()
